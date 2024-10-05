7b0d11c72364fb31d06d264ea5c9cf187ba110c8
F3cha <goncalofecha@gmail.com>
2024-10-06 00:12:24 +0100

    first classes!

---
+++ hva-skeleton/hva/core/EvergreenTree.java
@@ -0,0 +1,12 @@
+package hva.core;
+
+public class EvergreenTree extends Tree {
+
+    public EvergreenTree(Season season, int age, int difficulty) {
+        super(season, age, difficulty);
+    }
+
+    public void cleaningEffort() {
+        //meter a função
+    }
+}
