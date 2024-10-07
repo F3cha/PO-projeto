dc4654b0962c09eb113642f8e41d3a8b8d8268fd
joaopmendonca04 <134430333+joaopmendonca04@users.noreply.github.com>
2024-10-07 19:07:51 +0100

    Add files via upload

---
+++ hva-skeleton/hva/core/Habitat.java
@@ -0,0 +1,18 @@
+package hva.core;
+
+public class Habitat {
+    private String _habitatId;
+    private String _habitatName;
+    private int _area;
+
+
+    public Habitat(String habitatId, String habitatName, int area) {
+        _habitatId = habitatId;
+        _habitatName = habitatName;
+        _area = area;
+    }
+
+    public String getHabitatId() {
+        return _habitatId;
+    }
+}
