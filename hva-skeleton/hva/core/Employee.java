fc562ec104f2e072ed70cdb978034bf214a90eb9
joaopmendonca04 <134430333+joaopmendonca04@users.noreply.github.com>
2024-10-08 23:03:20 +0100

    Update Employee.java

--- hva-skeleton/hva/core/Employee.java
+++ hva-skeleton/hva/core/Employee.java
@@ -1,8 +1,11 @@
 package hva.core;
 
+import java.util.List;
+
 abstract class Employee {
     private String _idEmployee;
     private String _nameEmployee;
+    private List<String> _responsibility;
 
     public Employee(String idEmployee, String nameEmployee) {
         _idEmployee = idEmployee;
@@ -11,6 +14,14 @@ abstract class Employee {
 
     public String getEmployeeId() {
         return _idEmployee;
+    }
+
+    public List<String> getResponsibility() {
+        return _responsibility;
+    }
+
+    public void setResponsability(List<String> responsibility) {
+        _responsibility.addAll(responsibility);
     }
 
     abstract int satisfaction();
